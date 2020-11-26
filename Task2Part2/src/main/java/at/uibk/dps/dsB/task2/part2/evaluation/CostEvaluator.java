a3a6accaeb822e0eb8b4b36d533daaa927c2f3bf
Simon Kleinfeld <simon.kleinfeld@eisenmann.com>
2020-11-26 21:13:53 +0100

    added costs and makespan method

--- Task2Part2/src/main/java/at/uibk/dps/dsB/task2/part2/evaluation/CostEvaluator.java
+++ Task2Part2/src/main/java/at/uibk/dps/dsB/task2/part2/evaluation/CostEvaluator.java
@@ -1,48 +1,77 @@
 package at.uibk.dps.dsB.task2.part2.evaluation;
 
-import org.opt4j.core.Objective;
-import org.opt4j.core.Objectives;
-
-import org.opt4j.core.Objective.Sign;
-
 import at.uibk.dps.dsB.task2.part2.properties.PropertyProvider;
 import at.uibk.dps.dsB.task2.part2.properties.PropertyProviderStatic;
+import at.uibk.dps.dsB.task2.part2.properties.PropertyService;
+import java.util.concurrent.atomic.AtomicReference;
 import net.sf.opendse.model.Specification;
 import net.sf.opendse.optimization.ImplementationEvaluator;
+import org.opt4j.core.Objective;
+import org.opt4j.core.Objective.Sign;
+import org.opt4j.core.Objectives;
 
 /**
- * The {@link CostEvaluator} is used to calculate the costs of different
- * orchestrations of the PIW3000.
- * 
- * @author Fedor Smirnov
+ * The {@link CostEvaluator} is used to calculate the costs of different orchestrations of the PIW3000.
  *
+ * @author Fedor Smirnov
  */
-public class CostEvaluator implements ImplementationEvaluator {
+public class CostEvaluator
+        implements ImplementationEvaluator
+{
 
-	protected final Objective costObjective = new Objective("Costs [Distopistan Dorrar]", Sign.MIN);
-	protected final PropertyProvider propertyProvider = new PropertyProviderStatic();
+    private final Objective costObjective = new Objective("Costs [Distopistan Dorrar]", Sign.MIN);
+    protected final PropertyProvider propertyProvider = new PropertyProviderStatic();
 
-	@Override
-	public Specification evaluate(Specification implementation, Objectives objectives) {
-		double costs = calculateImplementationCost(implementation);
-		objectives.add(costObjective, costs);
-		// No changes to the implementation => return null
-		return null;
-	}
+    @Override
+    public Specification evaluate(Specification implementation, Objectives objectives)
+    {
+        double costs = calculateImplementationCost(implementation);
+        objectives.add(costObjective, costs);
+        // No changes to the implementation => return null
+        return null;
+    }
 
-	/**
-	 * Does the actual cost calculation
-	 * 
-	 * @param implementation the solution which is being evaluated
-	 * @return the cost of the implementation
-	 */
-	protected double calculateImplementationCost(Specification implementation) {
-		throw new IllegalStateException("Cost Calculation not yet implemented.");
-	}
+    /**
+     * Does the actual cost calculation
+     *
+     * @param implementation the solution which is being evaluated
+     * @return the cost of the implementation
+     */
+    private double calculateImplementationCost(Specification implementation)
+    {
+        //from lib file
+        AtomicReference<Double> resCost = new AtomicReference<>(0.0D);
 
-	@Override
-	public int getPriority() {
-		// To be executed after the timing evaluator
-		return TimingEvaluator.priority + 1;
-	}
+        implementation.getArchitecture()
+                .forEach(res -> {
+                    if ( PropertyService.isCloudResource(res) )
+                    {
+                        double costRate = PropertyService.getResourceCosts(res);
+                        double accumulatedTime = res.getAttribute("Accumulated Usage");
+                        var multiply = costRate * accumulatedTime;
+                        resCost.updateAndGet(v -> (v + multiply));
+
+                    }
+                    else
+                    {
+                        resCost.updateAndGet(v -> (v + PropertyService.getResourceCosts(res)));
+                    }
+
+                });
+
+        double linkCosts = implementation.getArchitecture()
+                .getEdges()
+                .stream()
+                .mapToDouble(PropertyService::getLinkCost)
+                .sum();
+
+        return resCost.get() + linkCosts;
+    }
+
+    @Override
+    public int getPriority()
+    {
+        // To be executed after the timing evaluator
+        return TimingEvaluator.priority + 1;
+    }
 }
--- Task2Part2/src/main/java/at/uibk/dps/dsB/task2/part2/evaluation/TimingEvaluator.java
+++ Task2Part2/src/main/java/at/uibk/dps/dsB/task2/part2/evaluation/TimingEvaluator.java
@@ -1,49 +1,237 @@
 package at.uibk.dps.dsB.task2.part2.evaluation;
 
+import at.uibk.dps.dsB.task2.part2.properties.PropertyProvider;
+import at.uibk.dps.dsB.task2.part2.properties.PropertyProviderStatic;
+import at.uibk.dps.dsB.task2.part2.properties.PropertyService;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import net.sf.opendse.model.Application;
+import net.sf.opendse.model.Architecture;
+import net.sf.opendse.model.Communication;
+import net.sf.opendse.model.Dependency;
+import net.sf.opendse.model.Link;
+import net.sf.opendse.model.Mapping;
+import net.sf.opendse.model.Mappings;
+import net.sf.opendse.model.Resource;
+import net.sf.opendse.model.Specification;
+import net.sf.opendse.model.Task;
+import net.sf.opendse.model.properties.TaskPropertyService;
+import net.sf.opendse.optimization.ImplementationEvaluator;
 import org.opt4j.core.Objective;
 import org.opt4j.core.Objective.Sign;
 import org.opt4j.core.Objectives;
 
-import at.uibk.dps.dsB.task2.part2.properties.PropertyProvider;
-import at.uibk.dps.dsB.task2.part2.properties.PropertyProviderStatic;
-import net.sf.opendse.model.Specification;
-import net.sf.opendse.optimization.ImplementationEvaluator;
-
 /**
  * Evaluator for the makespan of the Piw3000
- * 
+ *
  * @author Fedor Smirnov
  */
-public class TimingEvaluator implements ImplementationEvaluator {
+public class TimingEvaluator
+        implements ImplementationEvaluator
+{
 
-	protected final PropertyProvider propertyProvider = new PropertyProviderStatic();
+    private final PropertyProvider propertyProvider = new PropertyProviderStatic();
 
-	protected static final int priority = 0;
+    static final int priority = 0;
 
-	protected final Objective makeSpanObjective = new Objective("Makespan [TU]", Sign.MIN);
+    private final Objective makeSpanObjective = new Objective("Makespan [TU]", Sign.MIN);
 
-	protected final String endTimeAttribute = "End Time";
-	public static final String accumulatedUsageAttribute = "Accumulated Usage";
+    protected final String endTimeAttribute = "End Time";
+    private static final String accumulatedUsageAttribute = "Accumulated Usage";
 
-	@Override
-	public Specification evaluate(Specification implementation, Objectives objectives) {
-		objectives.add(makeSpanObjective, calculateMakespan(implementation));
-		// Implementation annotated => return the impl
-		return implementation;
-	}
+    @Override
+    public Specification evaluate(Specification implementation, Objectives objectives)
+    {
+        objectives.add(makeSpanObjective, calculateMakespan(implementation));
+        // Implementation annotated => return the impl
+        return implementation;
+    }
 
-	/**
-	 * Does the actual makespan calculation.
-	 * 
-	 * @param implementation the orchestration under evaluation
-	 * @return the makespan of the orchestration
-	 */
-	protected double calculateMakespan(Specification implementation) {
-		throw new IllegalArgumentException("Makespan calculation not yet implemented.");
-	}
+    /**
+     * Does the actual makespan calculation.
+     *
+     * @param implementation the orchestration under evaluation
+     * @return the makespan of the orchestration
+     */
+    private double calculateMakespan(Specification implementation)
+    {
+        Application<Task, Dependency> appl = implementation.getApplication();
+        var toDo = new HashSet<>(appl.getVertices());
+        var startTimes = new HashMap<Task, Double>();
+        var endTimes = new HashMap<Task, Double>();
 
-	@Override
-	public int getPriority() {
-		return priority;
-	}
+        Task root = null;
+
+        for ( Task t : appl )
+        {
+            if ( appl.getPredecessorCount(t) == 0 )
+            {
+                root = t;
+                break;
+            }
+        }
+
+        var schedulable = new HashSet<Task>();
+        schedulable.add(root);
+        startTimes.put(root, 0.0D);
+        double result = 0.0D;
+
+        label55:
+        while ( !toDo.isEmpty() )
+        {
+            var var10 = schedulable.iterator();
+
+            while ( true )
+            {
+                Task t;
+                do
+                {
+                    if ( !var10.hasNext() )
+                    {
+                        toDo.removeAll(endTimes.keySet());
+                        var10 = toDo.iterator();
+
+                        while ( var10.hasNext() )
+                        {
+                            t = var10.next();
+                            if ( this.isSchedulable(t, endTimes, appl) )
+                            {
+                                schedulable.add(t);
+                            }
+                        }
+                        continue label55; //TODO remove label55
+                    }
+
+                    t = var10.next();
+                } while ( !toDo.contains(t) );
+
+                double execTime = TaskPropertyService.isProcess(t)
+                        ? this.getExecTimeTask(t,
+                                               implementation.getMappings())
+                        : this.getExecTimeComm((Communication) t,
+                                               implementation.getRoutings()
+                                                       .get(t));
+                double start = startTimes.get(t);
+                double endTime = start + execTime;
+                result = Math.max(result, endTime);
+                endTimes.put(t, endTime);
+
+                for ( Task succ : appl.getSuccessors(t) )
+                {
+                    this.updateSuccessorStartTime(endTime, startTimes, succ);
+                }
+
+                t.setAttribute(endTimeAttribute, endTime);
+            }
+        }
+
+        return result;
+    }
+
+    private void updateSuccessorStartTime(double endTimePred, Map<Task, Double> startTimes, Task successor)
+    {
+        if ( !startTimes.containsKey(successor) || startTimes.get(successor) < endTimePred )
+        {
+            startTimes.put(successor, endTimePred);
+        }
+
+    }
+
+    private double getExecTimeTask(Task t, Mappings<Task, Resource> mappings)
+    {
+        Set<Mapping<Task, Resource>> ms = mappings.get(t);
+        if ( ms.size() != 1 )
+        {
+            throw new IllegalArgumentException("More than one mapping for task.");
+        }
+        else
+        {
+            Mapping<Task, Resource> m = ms.iterator()
+                    .next();
+            double processingOneTask = this.propertyProvider.getExecutionTime(m);
+            int execNumber = this.getInstanceNumber(t);
+            Resource bindingTarget = m.getTarget();
+            this.updateUsage(bindingTarget, (double) execNumber * processingOneTask);
+            if ( PropertyService.isSingleExecTask(t) )
+            {
+                return processingOneTask;
+            }
+            else
+            {
+                int availableRes = PropertyService.isCloudResource(bindingTarget)
+                        ? this.propertyProvider.getNumberOfAvailableInstances(bindingTarget)
+                        : 1;
+                int runs = execNumber / availableRes + (execNumber % availableRes > 0 ? 1 : 0);
+                return (double) runs * processingOneTask;
+            }
+        }
+    }
+
+    private void updateUsage(Resource res, double usage)
+    {
+        double toSet = usage;
+        if ( res.getAttribute(accumulatedUsageAttribute) != null )
+        {
+            toSet = usage + (Double) res.getAttribute(accumulatedUsageAttribute);
+        }
+
+        res.setAttribute(accumulatedUsageAttribute, toSet);
+    }
+
+    private double getExecTimeComm(Communication comm, Architecture<Resource, Link> routing)
+    {
+        double transmissionTimeOneMessage = 0.0D;
+
+        Link l;
+        for ( var var5 = routing.getEdges()
+                .iterator(); var5.hasNext(); transmissionTimeOneMessage += this.propertyProvider.getTransmissionTime(
+                comm,
+                l) )
+        {
+            l = var5.next();
+        }
+
+        return transmissionTimeOneMessage * (double) this.getInstanceNumber(comm);
+    }
+
+    private int getInstanceNumber(Task t)
+    {
+        if ( PropertyService.isSingleExecTask(t) )
+        {
+            return 1;
+        }
+        else
+        {
+            return PropertyService.isIterativeCars(t)
+                    ? this.propertyProvider.getCarNumber()
+                    : this.propertyProvider.getNumberOfPeople();
+        }
+    }
+
+    private boolean isSchedulable(Task t, Map<Task, Double> endTimes, Application<Task, Dependency> appl)
+    {
+        var var4 = appl.getPredecessors(t)
+                .iterator();
+
+        Task pred;
+        do
+        {
+            if ( !var4.hasNext() )
+            {
+                return true;
+            }
+
+            pred = var4.next();
+        } while ( endTimes.containsKey(pred) );
+
+        return false;
+    }
+
+    @Override
+    public int getPriority()
+    {
+        return priority;
+    }
 }
